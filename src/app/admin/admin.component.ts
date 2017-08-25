36043e318442c466fa311ceb0be1ae8fc23fee26
Calla Rudolph <callarudolph@gmail.com>
2017-08-25 09:38:27 -0700

    create form to add new member

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-admin',
@@ -12,4 +13,7 @@ export class AdminComponent implements OnInit {
   ngOnInit() {
   }
 
+  submitForm(name: string, bio: string, cats: number, picture: string, location: string) {
+    var newMember: Member = new Member(name, bio, cats, picture, location);
+  }
 }
