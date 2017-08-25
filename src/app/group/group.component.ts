a4d648b208f77e5520e951a75ee9da7c9925fa28
Calla Rudolph <callarudolph@gmail.com>
2017-08-25 09:18:33 -0700

    create member service

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
+import { Router } from '@angular/router';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-group',
   templateUrl: './group.component.html',
-  styleUrls: ['./group.component.css']
+  styleUrls: ['./group.component.css'],
+  providers: [MemberService]
 })
 export class GroupComponent implements OnInit {
-  members: Member[]; 
+  members: Member[];
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
   }
---
+++ src/app/member.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Member } from './member.model';
+
+@Injectable()
+export class MemberService {
+
+  constructor() { }
+
+}
