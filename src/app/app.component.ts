722df734a72f1c0336341ce4895a7334a1b78025
Calla Rudolph <callarudolph@gmail.com>
2017-08-25 16:12:33 -0700

    add ngOnInit functionality to count and display number of total messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { MemberService } from './member.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { Member } from './member.model';
+import { Messages } from './messages.model';
+import { MessagesService } from './messages.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [MemberService]
+  providers: [MemberService, MessagesService]
 })
 export class AppComponent {
   title = 'Cat Fanciers';
   currentDate = new Date();
   members: FirebaseListObservable<any[]>;
+  messages: FirebaseListObservable<any[]>;
   memberList;
   totalCats = 0;
   totalMembers = 0;
+  totalMessages = 0;
+  messagesList;
 
-  constructor(private memberService: MemberService) { }
+  constructor(private memberService: MemberService, private messagesService: MessagesService) { }
 
   ngOnInit() : void {
     this.memberService.getMembers().subscribe(dataLastEmittedFromObserver => {
@@ -31,6 +36,13 @@ export class AppComponent {
       this.totalCats = cats;
       this.totalMembers = counter;
     });
-
+    this.messagesService.getMessages().subscribe(dataLastEmittedFromObserver => {
+      var counter = 0;
+      this.messagesList = dataLastEmittedFromObserver;
+      this.messagesList.forEach(function(currentMessage) {
+        counter++;
+      })
+      this.totalMessages = counter;
+    })
   }
 }
