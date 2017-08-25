632cf70c1a46c923cece6d27bcf9b44e3ff46ec6
Calla Rudolph <callarudolph@gmail.com>
2017-08-25 11:38:17 -0700

    add observable to display total members and number of cats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MemberService } from './member.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Member } from './member.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MemberService]
 })
 export class AppComponent {
   title = 'Cat Fanciers';
   currentDate = new Date();
+  members: FirebaseListObservable<any[]>;
+  memberList;
+  totalCats = 0;
+  totalMembers = 0;
+
+  constructor(private memberService: MemberService) { }
+
+  ngOnInit() : void {
+    this.memberService.getMembers().subscribe(dataLastEmittedFromObserver => {
+      var cats: number = 0;
+      var counter = 0;
+      this.memberList = dataLastEmittedFromObserver;
+      this.memberList.forEach(function(currentMember) {
+        cats += parseInt(currentMember.cats);
+        counter ++;
+      })
+      this.totalCats = cats;
+      this.totalMembers = counter;
+    });
+
+  }
 }
